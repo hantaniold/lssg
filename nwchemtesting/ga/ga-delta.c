8e137cd24836b83efaf843330cc683bc7785232a
SeanHogan <seagaia@f-m.fm>
2012-02-25 14:48:20 -0600

    Added recovery to GA delta. need to verify correctness again

--- nwchemtesting/ga/ga-delta.c
+++ nwchemtesting/ga/ga-delta.c
@@ -15,6 +15,7 @@
 
 #define EXP_MASK 2047 //0b11111111111
 #define FRAC_MASK (-1L ^ ((1L << 63) >> 11)) //0b00000000000011....
+#define MSB_MASK (1L << 63)
 #define BIAS 1023
 #define FRAC_WIDTH 52
 #define EXP_WIDTH 11
@@ -39,6 +40,7 @@ struct gctx_t {
 	FILE * outFP;
 	unsigned long int minSize;
 	unsigned long int subSize;
+	unsigned long int delSize;
 	int thresholdExp;
 	int doFilter;
 };
@@ -105,6 +107,7 @@ int main(int argc, char * argv[]) {
 	if (doRecover) {
 		recoverMinuend(gctx);
 	} else {
+	
 		computeDelta(gctx);
 	}
 	
@@ -148,7 +151,7 @@ void computeDelta(struct gctx_t * gctx) {
 	ga_min = NGA_Create(C_DBL, 1, dims, "Minuend Array", chunk);
 	if (!ga_min) GA_Error("Failure to create minuend array", 1);
 	if (my_id == 0) printf("=== Created Minuend Array.\n");
-	ga_sub = GA_Duplicate(ga_min,"Minuend Array");
+	ga_sub = GA_Duplicate(ga_min,"Subtrahend Array");
 	ga_out = GA_Duplicate(ga_min,"Output Array");
 
 	if (!ga_sub) GA_Error("duplicating min -> sub failed.",1);
@@ -304,5 +307,163 @@ void computeDelta(struct gctx_t * gctx) {
 }
 
 void recoverMinuend(struct gctx_t *gctx) {
+	int my_id = GA_Nodeid();
+	int nrProcs = GA_Nnodes();
 
+/* Find file sizes */
+	struct stat * delStat = calloc(1,sizeof(struct stat));
+	struct stat * subStat = calloc(1,sizeof(struct stat));
+	fstat(fileno(gctx->delFP),delStat);
+	fstat(fileno(gctx->subFP),subStat);
+	if (my_id == 0) {
+		printf("Delta size: %ld\n",delStat->st_size);
+		printf("Subtrahend size: %ld\n",subStat->st_size);
+	}
+	if (delStat->st_size != subStat->st_size) GA_Error("Error: Delta and Subtrahend not same size", 1);	
+	gctx->delSize = delStat->st_size;
+	gctx->subSize = subStat->st_size;
+	
+
+/* Create the global arrays */
+	int dims[1], chunk[1];
+	int ga_del, ga_sub, ga_out;
+
+	dims[0] = gctx->delSize/8;
+	chunk[0] = dims[0]/nrProcs - 1;
+	ga_del = NGA_Create(C_DBL, 1, dims, "Delta Array", chunk);
+	if (!ga_del) GA_Error("Failure to create minuend array", 1);
+	if (my_id == 0) printf("=== Created Minuend Array.\n");
+	ga_sub = GA_Duplicate(ga_del,"Subtrahend Array");
+	ga_out = GA_Duplicate(ga_del,"Output Array");
+
+	if (!ga_sub) GA_Error("duplicating del -> sub failed.",1);
+	if (!ga_out) GA_Error("duplicating del -> out failed.",1);
+	if (my_id == 0) printf("=== Created Subtrahend and Output Array.\n");
+	
+	
+/* Read the minuend and subtrahend into local buffers, copy this to global array.
+ * Maybe split to two processors o_O */
+	if (my_id == 0) {
+		int globalStride[1];
+		globalStride[0] = dims[0];
+		printf("Hi from %d\n",my_id);
+		printf("%d\n",dims[0]);
+		double * delDoubles = calloc(dims[0],sizeof(double));
+		printf("%d\n",dims[0]);
+		
+		double * subDoubles = calloc(dims[0],sizeof(double));
+		unsigned long int i = 0;
+		char * buf1 = (char *) malloc(8);
+		char * buf2 = (char *) malloc(8);
+		printf("=== Filling del/sub array on processor %d\n",my_id);
+		while (fscanf(gctx->delFP,"%8c", buf1) != EOF && fscanf(gctx->subFP, "%8c", buf2) != EOF) {
+			delDoubles[i] = *((double *) buf1);
+			subDoubles[i] = *((double *) buf2);
+			i++;
+		}	
+		if (i != dims[0]) GA_Error("Did not read as many doubles as expected from subtrahend",1);
+		printf("=== Filled del/sub arrays on processor %d\n",my_id);
+		int lo[1]; lo[0] = 0;
+		int hi[1]; hi[0] = dims[0] - 1;
+		printf("Hi from line 183\n");
+		NGA_Put(ga_sub, lo, hi, subDoubles, globalStride);
+		NGA_Put(ga_del, lo, hi, delDoubles, globalStride);
+		free(subDoubles);
+		free(delDoubles);
+	}
+
+
+	GA_Sync();
+
+	if (my_id == 0) printf("=== Transferring global doubles to local arrays.\n");
+/* Move global doubles to local arrays */ 
+	int lo[1], hi[1];
+	NGA_Distribution(ga_out, my_id, lo,hi);
+	int stride[1]; stride[0] = hi[0] - lo[0] + 1;
+	
+	double * localDelDoubles = calloc(stride[0],sizeof(double));
+	NGA_Get(ga_del, lo, hi, localDelDoubles, stride);
+	
+	double * localSubDoubles = calloc(stride[0],sizeof(double));
+	NGA_Get(ga_sub, lo, hi, localSubDoubles, stride); 
+
+	if (my_id == 0) printf("=== Starting delta computation\n");	
+
+	char subBuf[8]; char delBuf[8];
+	double dSub;	double dDel;
+	double dMin;
+	int shamt;
+	long int em;
+	long int ed;
+	int ed_MINUS_em_isPOS;
+	int ed_MINUS_em;
+	long int fd;
+	long int dDelBits;
+	double * localOutDoubles = calloc(stride[0], sizeof(double));
+
+	int j = 0;
+	printf("proc %d entering recovery\n",my_id);
+	for (j = 0; j < stride[0]; j++) {
+		dDel = localDelDoubles[j];
+		dSub = localSubDoubles[j];
+		if (dDel == 0) {
+			if (dDel && MSB_MASK) {
+				localOutDoubles[j] = 0;
+				continue;
+			} else {
+				localOutDoubles[j] = dSub;
+				continue;
+			}
+		}
+		dDelBits = *((long int *) &dDel);
+
+		dDelBits = dDelBits & FRAC_MASK;
+		dDelBits <<= 12;
+		shamt = 0;
+		while ((dDelBits & MSB_MASK) != MSB_MASK) {
+			shamt++;
+			dDelBits <<= 1;	
+		}
+		ed_MINUS_em_isPOS = ((dDelBits << 1) & MSB_MASK > 0);
+		if (ed_MINUS_em_isPOS) {
+			ed_MINUS_em = shamt;
+		} else { 
+			ed_MINUS_em = -1 * shamt;
+		}
+		dDelBits = *((long int *) &dDel);
+		em = ((dDelBits >> FRAC_WIDTH) & EXP_MASK) - BIAS;
+		ed = ed_MINUS_em + em;
+		ed += BIAS;
+		ed <<= FRAC_WIDTH;
+		
+		dDelBits = *((long int *) &dDel);
+		dDelBits &= FRAC_MASK;
+		fd = dDelBits << (shamt + 1);
+		
+		dDelBits = *((long int *) &dDel);
+		dDelBits &= MSB_MASK;
+		dDelBits |= fd;
+		dDelBits |= ed;
+		dMin = dSub + *((double *) &dDelBits);	
+		localOutDoubles[j] = dMin;
+	}
+	
+	printf("Proc %d computed %d deltas.\n",my_id,j);
+	if (my_id == 0) printf("=== Moving recovered minuend values to global array.\n");
+	NGA_Put(ga_out, lo, hi, localOutDoubles, stride);
+	GA_Sync();
+	if (my_id == 0) {
+		double * outDoubles = calloc(dims[0],sizeof(double));
+		lo[0] = 0; hi[0] = dims[0] - 1; stride[0] = dims[0];
+		NGA_Get(ga_out, lo, hi, outDoubles, stride);
+		for (j = 0; j < dims[0]; j++) {
+			fwrite(&(outDoubles[j]), sizeof(double), 1, gctx->outFP);
+		}
+	}
+	if (my_id == 0) printf("Deltas written to specified output file.\n");
+
+
+	GA_Destroy(ga_del);
+	GA_Destroy(ga_out);
+	GA_Destroy(ga_sub);
 }
