3949bb182dd54d523d9effc2385f022ad8eeb226
SeanHogan <seagaia@f-m.fm>
2012-03-02 15:10:14 -0600

    update test framework

--- nwchemtesting/ga/clean.py
+++ nwchemtesting/ga/clean.py
@@ -6,17 +6,19 @@ import sys
 res = raw_input("The input should be a list of TCE input \n \
 with 5 lines of info we don't need, then lines of [number] [some double].\n \
  The output will be all of the input filenames plus \".bin\" appended, \n \
- written as doubles for input into the GA program.\n \
+ written as doubles for input into the GA program.\n\n Also the 2nd arg should be the directory holding these files. \
  If this is okay type \"y\"\n")
 
 if res != "y":
     exit(0)
 
 filenames = open(sys.argv[1],"r")
+inputdir = sys.argv[2]
+inputdir = inputdir.rstrip("/")
 
 for filename in filenames:
     # Remove the leading 5 lines
-    filename = filename.rstrip('\n')
+    filename = inputdir+"/"+filename.rstrip('\n')
     os.system("tail -n +6 "+filename+" > "+filename+"mytmp")
     # Rename the temp file to the original file.
     # Convert into binary using ga2bin
--- nwchemtesting/ga/runtests.py
+++ nwchemtesting/ga/runtests.py
@@ -9,11 +9,69 @@ import sys
 
 ### MAIN ###
 
+if (len(sys.argv) < 3):
+    print("usage: [input file list] [input dir] [output dir] [output prefix] <-np #nrprocs> <-f cutoff exponent>");
+    exit(0)
+
 inputFileHandle = open(sys.argv[1],"r")
 inputFilesList = []
 for line in inputFileHandle:
     inputFilesList += [line.rstrip("\n")]
 
+inputDir = sys.argv[2].rstrip("/")+"/"
+outputDir = sys.argv[3].rstrip("/")+"/"
+outputPrefix = sys.argv[4]
+
+
+np = 1
+cutoff = ""
+# Parse rest of the optional params
+for i in range(4,len(sys.argv)):
+    if (sys.argv[i] == "-np"):
+        np = int(sys.argv[i+1])
+    elif (sys.argv[i] == "-f"):
+        cutoff = " -f "+sys.argv[i+1]+" "
+
+
+print "=================================="
+print "list of input files: "+sys.argv[1]
+print "input dir: "+sys.argv[2]
+print "output directory: "+sys.argv[3]
+print "output prefix: "+sys.argv[4]
+print "cutoff args: "+cutoff
+print "nr procs: "+str(np)
+print "=================================="
+ans = raw_input("enter y if this is okay\n>>> ")
+if (ans != "y"):
+  print("Exiting.")
+  exit(0)
+
+
 #Compute deltas of 2-1, 3-2
 #Compress/decompress these deltas and time and whatnot
 #Compute recovery and do the times or something and check sizes
+
+
+mpiCmd = "mpirun -np "+str(np)+" -machinefile machines.txt ./ga-delta.x "+cutoff
+
+# Run delta compression. Write out files with successive
+# numeric prefixes: outputPrefix.n-n-1.delta
+for i in range(len(inputFilesList) - 1):
+    subt = "-s "+inputDir+inputFilesList[i]+".bin "
+    minu = "-m "+inputDir+inputFilesList[i+1]+".bin "
+    output =  "-o "+outputDir+outputPrefix+"."+str(i+2)+"-"+str(i+1)+".del"
+    print(mpiCmd+subt+minu+output)
+    os.system(mpiCmd+subt+minu+output)
+    
+# Compress the files.
+# Decompress.
+# Recover.
+
+for i in range(len(inputFilesList) - 1):
+    subt = "-s "+inputDir+inputFilesList[i]+".bin "
+    delt = "-d "+outputDir+outputPrefix+"."+str(i+2)+"-"+str(i+1)+".del "
+    output = "-o "+outputDir+inputFilesList[i+1]+".rec"
+    os.system(mpiCmd+" -r "+subt+delt+output)
+    print(mpiCmd+" -r "+subt+delt+output)
+
+    
